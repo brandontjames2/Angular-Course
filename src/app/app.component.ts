872b8d5548fd39a3e89c6eb12c0ac4c8e1d0d976
Brandon James <brandon.james@slalom.com>
2019-12-16 14:30:37 -0500

    committing project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  styles: [`
+  h3 {
+    color: lightblue;
+  }`,
+  `.whiteText {
+    color: white;
+  }`
+  ]
 })
 export class AppComponent {
+  username = "";
+  displayDetails = false;
+  timesClicked = [];
+
+  onReset() {
+    this.username = "";
+  }
+
+  onDisplayDetails() {
+    this.displayDetails = !this.displayDetails;
+    this.timesClicked.push(new Date(new Date().getTime()));
+  }
 }
